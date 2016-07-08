067285075ae5712c309a71e8b21bc7fc0358230b
Brian Bolli <brian.bolli@arctg.com>
2016-07-08 14:11:09 -0500

    Initial code commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,66 +8,75 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'tasks/*.js',
+				'<%= nodeunit.tests %>'
+			],
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		},
+		// Before generating any new files, remove any previously-created files.
+		clean: {
+			tests: ['tmp']
+		},
+		// Configuration to be run (and then tested).
+		rwd: {
+			default_options: {
+				options: {
+				},
+				links: []
+			},
+			custom_options: {
+				options: {
+					destination: "./screenshots/",
+					breakpoints: [
+						{
+							name: "smartdevice",
+							width: 1024,
+							height: 768
+						},
+						{
+							name: "desktop",
+							width: 1280,
+							height: 1024
+						}
+					]
+				},
+				links: [
+					{
+						name: "Google",
+						target: "http://google.com"
+					}
+				]
+			}
+		},
+		// Unit tests.
+		nodeunit: {
+			tests: ['test/*_test.js']
+		}
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
+	});
 
-    // Configuration to be run (and then tested).
-    rwd: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      }
-    },
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  });
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
+	// plugin's task(s), then test the result.
+	grunt.registerTask('test', ['clean', 'rwd', 'nodeunit']);
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
-
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'rwd', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+	// By default, lint and run all tests.
+	grunt.registerTask('default', ['jshint', 'test']);
 
 };
